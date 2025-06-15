a2c5f1b2f1ecc0487e207ebd4cb70aed53c6a9d8
Senrism <lasenafebry@gmail.com>
2025-06-15 22:52:06 +0700

    change asset using canva design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,16 @@
 import Image from "next/image";
 import './style.css';
 import { useEffect, useRef, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
 
 export default function Home() {
 
   const audioRef = useRef<HTMLAudioElement>(null);
-  let isPlaying = false;
   const [isVisible, setIsVisible] = useState(true);
   const [isHiding, setIsHiding] = useState(false);
+
+  const searchParams = useSearchParams();
+  const to = searchParams.get('to');
 
   let messages = [
     {
@@ -33,20 +36,26 @@ export default function Home() {
     },
   ]
 
+  const [showContent, setShowContent] = useState(false)
   const handleOpen = () => {
     setIsHiding(true); // mulai fade-out
     setTimeout(() => {
       setIsVisible(false); // benar-benar hilang dari DOM
-      audioRef.current?.play(); // play audio setelah hilang
+      const video = videoRef.current
+      if (video) {
+        video.play(); // play video setelah fade-out selesai
+        // const handleEnded = () => setShowContent(true)
+        // video.addEventListener('ended', handleEnded)
+        // return () => video.removeEventListener('ended', handleEnded)
+      }
+      // audioRef.current?.play(); // play audio setelah hilang
     }, 500); // waktu sama dengan transition duration
-  };
-
-  const handlePlay = () => {
-    audioRef.current?.play();
   };
 
   const divRef = useRef<HTMLDivElement | null>(null);
   const scrollAmount = useRef(0);
+
+  const videoRef = useRef<HTMLVideoElement>(null)
 
   useEffect(() => {
     let animationFrameId:number;
@@ -72,25 +81,42 @@ export default function Home() {
 
   return (
     <div>
-      <audio ref={audioRef} src="/bg-music-2.mp3" loop autoPlay onPlay={handlePlay} className="hidden">
-      </audio>
+      {/* <audio ref={audioRef} src="/bg-music-2.mp3" loop autoPlay onPlay={handlePlay} className="hidden">
+      </audio> */}
       {isVisible && (
         <div
-          className={`fixed top-0 left-0 w-full h-full bg-[#c2f0ea] z-50 flex items-center justify-center transition-opacity duration-500 ${
-            isHiding ? "opacity-0" : "opacity-50"
+          className={`fixed top-0 left-0 w-full h-full z-50 flex items-center justify-center transition-opacity duration-500 ${
+            isHiding ? "opacity-0" : "opacity-100"
           }`}
         >
-          <div className="bg-white p-6 rounded-lg shadow-lg text-center">
+          <Image
+            src="/wallpaper.jpg"
+            alt="Wedding Invitation"
+            fill
+            priority
+            className="object-cover z-0"
+          />
+
+          <div className="z-10">
+            <button
+              onClick={handleOpen}
+              className="bg-[#956850] text-white px-4 py-2 rounded hover:bg-[#F7F2EA] transition duration-300"
+            >
+              Buka Undangan untuk {to ? to : 'Tamu'}
+            </button>
+          </div>
+
+          {/* <div className="bg-white p-6 rounded-lg shadow-lg text-center">
             <button
               onClick={handleOpen}
               className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300"
             >
-              Buka Undangan
+              Buka Undangan untuk {to ? to : 'Tamu'}
             </button>
-          </div>
+          </div> */}
         </div>
       )}
-      <div
+      {/* <div
         className="bg-[url('/bg.jpg')] h-screen w-screen bg-cover bg-no-repeat bg-center flex justify-center relative"
       >
         <h2 style={{fontSize: '20px', color: 'white'}} className="text-center absolute top-10">
@@ -115,9 +141,18 @@ export default function Home() {
             <span className="text-white text-sm">Scroll Down</span>
         </div>
 
+      </div> */}
+
+      <div className="relative w-screen h-screen overflow-hidden">
+        <video
+          ref={videoRef}
+          src="/wedding-video-1.mp4" // Place this in /public folder
+          className="absolute w-full h-full object-cover"
+          playsInline
+        />
       </div>
 
-      <div className="flex flex-col items-center h-[500px] bg-gradient-to-b from-[#f8cfd8] via-[#d3d3d3] to-[#c2f0ea] px-6">
+      {/* <div className="flex flex-col items-center h-[500px] bg-gradient-to-b from-[#f8cfd8] via-[#d3d3d3] to-[#c2f0ea] px-6">
           <p className="text-white mt-[100px]">
             Allah's blessings message يَتَفَكَّرُونَ 
           </p>
@@ -129,20 +164,15 @@ export default function Home() {
           <p className="text-white mt-4">
             (Quran Surah Ar-Rum: 21)
           </p>
+      </div> */}
 
-      </div>
-
-      <div className="flex flex-col items-center h-screen bg-gradient-to-b from-[#c2f0ea] via-[#d3d3d3] to-[#f8cfd8] px-6">
-
+      {/* <div className="flex flex-col items-center h-screen bg-gradient-to-b from-[#c2f0ea] via-[#d3d3d3] to-[#f8cfd8] px-6">
         <h2 className="text-3xl font-bold text-center text-white">
           Wedding Ceremony
         </h2>
         <p className="text-lg text-center mt-4 text-white mb-[50px]">
             Jl. Veteran III Gg. Saung Wira I No.17, RT.02/RW.05, Banjar Waru, Kec. Ciawi, Kabupaten Bogor, Jawa Barat 16720
         </p>
-
-        {/* button to see location */}
-
         <a
           href="https://maps.app.goo.gl/bUTohvdAVu8pBTqV8"
           target="_blank"
@@ -159,27 +189,27 @@ export default function Home() {
           style={{ border: 0, width: '100%', height: '300px' }}
           loading="lazy">
         </iframe>
+      </div> */}
 
-      </div>
-
-      <div className="flex flex-col items-center h-screen bg-gradient-to-b from-[#c2f0ea] via-[#d3d3d3] to-[#f8cfd8] px-6">
+      {/* <div className="flex flex-col items-center h-screen bg-gradient-to-b from-[#c2f0ea] via-[#d3d3d3] to-[#f8cfd8] px-6">
         <div className="bg-white h-[300px] w-[100%] mt-[20px] rounded-xl px-2 overflow-y-scroll scrollable-div no-scrollbar" ref={divRef}>
           {messages.map((message, index) => (
             <div className="w-[100%] border-b-2 py-4" key={index}>
               <div className="w-[100%]">
-                <span>
+                <span className="text-black">
                   {message.sender}
                 </span>
               </div>
               <div className="w-[100%] py-2">
-                <span>
+                <span className="text-gray-700">
                   {message.message}
                 </span>
               </div>
             </div>
           ))}
         </div>
-      </div>
+      </div> */}
+
     </div>
   );
 }
